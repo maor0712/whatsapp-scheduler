5b88d3b6b8ac1a414635009555822d25c8c0f997
maor0712 <139145285+maor0712@users.noreply.github.com>
2023-07-17 16:51:27 +0300

    Delete front directory

--- front/contact/contact.js
+++
@@ -1,5 +0,0 @@
-function openWhatsAppChat() {
-    var number = "+972543384547"; // Replace with your desired phone number
-    var url = "https://wa.me/" + number;
-    window.open(url, '_blank');
-}
